e56464ca854d338f0c37be712dc33be7a14f3562
AndyMelR <ameleror@hotmail.es>
2023-03-02 13:40:38 +0000

    v1.0 ok

--- src/app.js
+++ src/app.js
@@ -2,10 +2,60 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document;
 };
+
+const getListaPalos = () => {
+  return ["❤️", "♦️", "♣️", "♠️"];
+};
+
+const getListNumber = () => {
+  return ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+};
+
+const getRandomIndex = array => {
+  return Math.floor(Math.random() * array.length);
+};
+
+const card = {
+  symbol: null,
+  number: null
+};
+
+const getRandomCard = () => {
+  const symbolRandom = getListaPalos()[getRandomIndex(getListaPalos())];
+  const numberRandom = getListNumber()[getRandomIndex(getListNumber())];
+  return { symbolRandom, numberRandom };
+};
+
+const displayCard = card => {
+  let cardHTML = "";
+  if (card.symbolRandom == "❤️" || card.symbolRandom == "♦️") {
+    cardHTML = `<div class="card">
+    <div class="card-body">
+      <div class="card-item"><span class="emoticon">${card.symbolRandom}</span></div>
+      <div class="card-item" style="color:red;display:flex;justify-content:center;"><span class="emoticon">${card.numberRandom}</span></div>
+      <div id="symbol-reverse" class="card-item transformed"><span class="emoticon">${card.symbolRandom}</span></div>
+    </div>
+  </div>`;
+  } else {
+    cardHTML = `<div class="card">
+      <div class="card-body">
+        <div class="card-item"><span class="emoticon">${card.symbolRandom}</span></div>
+        <div class="card-item" style="display:flex;justify-content:center;"><span class="emoticon">${card.numberRandom}</span></div>
+        <div id="symbol-reverse" class="card-item transformed"><span class="emoticon">${card.symbolRandom}</span></div>
+      </div>
+    </div>`;
+  }
+  const cardContainer = document.querySelector("#card-container");
+  cardContainer.innerHTML = cardHTML;
+};
+
+const generateRandomCard = () => {
+  const card = getRandomCard();
+  displayCard(card);
+};
+
+const generateCardBtn = document.querySelector("#generate-card-btn");
+generateCardBtn.addEventListener("click", generateRandomCard);
